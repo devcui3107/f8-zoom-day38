2cb18a46e974e08fc0b65a864f468fa6b08ec35a
devcui3107 <cudun6789@gmail.com>
2025-09-14 01:07:11 +0700

    Done Go To Top && ChangeAvatar

---
+++ src/components/GoToTop/index.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import clsx from "clsx";
+import Button from "../../components/Button";
+
+import styles from "./GoToTop.module.scss";
+import arrowUp from "/icons/arrow-up.svg";
+
+function GoToTop({ distance = 300 }) {
+  const [showIcon, setShowIcon] = useState(false);
+  const classNames = clsx(styles.scrollTopBtn, distance);
+
+  const returnTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" }); // Quay lên đầu trang
+  };
+
+  useEffect(() => {
+    const handleScrollTop = () => {
+      if (window.scrollY >= distance) {
+        setShowIcon(true);
+      } else if (window.scrollY < distance) {
+        setShowIcon(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScrollTop);
+
+    // Cleanup
+    return () => {
+      window.removeEventListener("scroll", handleScrollTop);
+    };
+  }, [distance]);
+
+  return (
+    showIcon && (
+      <Button className={classNames} onClick={returnTop}>
+        <img src={arrowUp} alt="Go to top" />
+      </Button>
+    )
+  );
+}
+
+export default GoToTop;
--- src/pages/ScrollDemo/index.jsx
+++ src/pages/ScrollDemo/index.jsx
@@ -1,5 +1,34 @@
+import GoToTop from "../../components/GoToTop";
+
+import styles from "./ScrollDemo.module.scss";
+
 function ScrollDemo() {
-  return <>Scroll Demo Page</>;
+  return (
+    <div className={styles.wrapper}>
+      <h1 className={styles.title}>Bài tập Scroll To Top</h1>
+      <p className={styles.intro}>
+        Trang này có rất nhiều nội dung để bạn cuộn xuống thử nghiệm tính năng
+        scroll lên đầu.
+      </p>
+
+      <div className={styles.content}>
+        {Array.from({ length: 40 }).map((_, index) => (
+          <p key={index}>
+            Đây là đoạn văn bản số {index + 1}. Lorem, ipsum dolor sit amet
+            consectetur adipisicing elit. Natus itaque deserunt quibusdam, quod
+            doloribus aspernatur cumque fugiat iure reiciendis atque! Quaerat
+            debitis possimus at hic nihil. Soluta, adipisci. Culpa distinctio
+            enim ipsam est laboriosam dolorum. Repellat cupiditate possimus iste
+            incidunt aspernatur provident animi, blanditiis adipisci maiores
+            rerum sed debitis hic.
+          </p>
+        ))}
+      </div>
+
+      {/* GoToTop */}
+      <GoToTop />
+    </div>
+  );
 }
 
 export default ScrollDemo;
