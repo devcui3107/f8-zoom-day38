c5498995c93add3dae8d63faef09173490b190c5
devcui3107 <cudun6789@gmail.com>
2025-09-13 15:56:03 +0700

    Fixed Path Logo

--- src/layouts/DefaultLayout/components/AppLogo/index.jsx
+++ src/layouts/DefaultLayout/components/AppLogo/index.jsx
@@ -1,16 +1,13 @@
 import { Link } from "react-router-dom";
 import styles from "./AppLogo.module.scss";
 import clsx from "clsx";
+import logo from "/images/logo-f8.png";
 
 function AppLogo({ white }) {
   return (
     <div className={styles.logo}>
       <Link to="/">
-        <img
-          src="/images/logo-f8.png"
-          alt="F8 Zoom Day 37"
-          className={styles.logoImage}
-        />
+        <img src={logo} alt="F8 Zoom Day 37" className={styles.logoImage} />
         <h1 className={clsx(styles.logoText, white && styles.white)}>
           Học lập trình để đi làm
         </h1>
